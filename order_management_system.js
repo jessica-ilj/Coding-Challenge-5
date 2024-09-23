71a903dceb173f1acddf29e0959988fe1d19c116
Jessica Ilok <jilok@usf.edu>
2024-09-22 21:07:24 -0400

    Initialize inventory with product objects

---
+++ order_management_system.js
@@ -0,0 +1,6 @@
+const inventory = [
+    { name: 'Espresso', price: 3, quantity: 10 }, //creating an  an Inventory Array of Product Objects with name, price and quantity
+    { name: 'Latte', price: 4, quantity: 5 },
+    { name: 'Cappuccino', price: 4, quantity: 6 },
+    { name: 'Mocha', price: 5, quantity: 4 }
+];
